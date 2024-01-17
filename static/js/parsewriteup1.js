2e14e20685f65501fe6e2979a0ed3d854fd16e10
missnhome <aixyctf@gmail.com>
2024-01-17 11:36:49 -0500

    2024.17.1.12

--- static/js/parsewriteup1.js
+++ static/js/parsewriteup1.js
@@ -223,31 +223,12 @@ writeupLinks=document.getElementsByClassName('writeup-link');
     //  Load the content of the first writeup on page load if have query
 if(hasQueryParam('query'))
   {
-// Assume you have a text box with the id "myTextBox"
+ 
 var myTextBox = document.getElementById("search-box");
 
 // Set the value of the text box
 var textToSimulate = getQueryParamOrDefault("query","");
 myTextBox.value = textToSimulate;
 
-// Create a new "Enter" key press event
-var enterKeyEvent = new KeyboardEvent("keydown", {
-    key: "Enter",
-    code: "Enter",
-    keyCode: 13,
-    which: 13,
-    bubbles: true,
-});
-
-// Attach an event listener to run a function when "Enter" is pressed
-myTextBox.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        // Call your function or perform actions here
-        console.log("Enter key pressed in text box");
-    }
-});
-
-// Dispatch the event to the text box
-myTextBox.dispatchEvent(enterKeyEvent);
-
+ 
     }
